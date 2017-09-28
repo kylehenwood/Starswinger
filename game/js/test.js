ea931270f3f0517a31907c82bfb19fb5b11565aa
Kyle <kyle.henwood@gmail.com>
2017-09-28 20:22:43 +1300

    made grid visible

--- game/js/test.js
+++ game/js/test.js
@@ -9,6 +9,7 @@ $(document).ready(function(){
     createHook();
 
     // RAF
+    drawGrid();
     testing();
 });
 
@@ -40,6 +41,7 @@ function setup() {
 }
 
 // controls
+var mouseState = null;
 function controls() {
   // mouse
   $(document).on('mousedown', function(e) {
@@ -67,21 +69,13 @@ function createHook() {
   starHooks.push({
       layer:hookCanvas,
       ctx: hookContext,
-      x: rand(0,800-64), // put these on a 64px grid
-      y: rand(0,400-64), // put these on a 64px grid + mark a square as filled
+      x: (rand(0,12)*64)-64, // put these on a 64px grid
+      y: (rand(0,4)*64)-64, // put these on a 64px grid + mark a square as filled
       power: 2000
   });
 }
 
-
-
-var ring;
-var stars;
-var mouseState = null;
-
-
 function drawHook(layer) {
-
   var star = {
     x: 32,
     y: 32,
@@ -106,32 +100,6 @@ function drawHook(layer) {
   layer.closePath();
   layer.stroke();
 
-  var radius = 25;
-  var startAngle = 0 * Math.PI;
-  var endAngle = ring * Math.PI
-  var counterClockwise = true;
-
-  // if (mouseState == 'down') {
-  //   if (ring <= 2) {
-  //     ring+=0.02;
-  //   } else {
-  //     return;
-  //     // destroy star
-  //   }
-  // }
-  //
-  // if (mouseState == 'up') {
-  //   ring = 0;
-  // }
-
-  endAngle = ring * Math.PI
-
-  layer.beginPath();
-  layer.lineWidth = 3;
-  layer.strokeStyle = 'red';
-  layer.arc(star.x, star.y, radius, startAngle, endAngle, counterClockwise);
-  layer.stroke();
-
   // visual bounds
   layer.beginPath();
   layer.lineWidth = 1;
@@ -152,6 +120,7 @@ function testing() {
   // Clear Canvas
   clear(canvas);
 
+  canvas.ctx.drawImage(gridImage,0,0);
 
   // draw layers
   for (var i = 0; i < starHooks.length; i++) {
@@ -161,26 +130,138 @@ function testing() {
     }
   }
 
-  // draw and update the star currently connected to
-  var layer = starHooks[0];
-  canvas.ctx.drawImage(layer.layer, layer.x, layer.y);
 
   // Currently hooked onto layer 1
   // figure out how to update a layer you've hooked onto...
-  // starGrappel(); // updates layer X
+
+  var grappled = starHooks[0];
+
+  starGrappel(grappled); // updates layer X
+  canvas.ctx.drawImage(grappled.layer, grappled.x, grappled.y);
 }
 
 
-function starGrappel() {
+
+var ring = 2;
+function starGrappel(grappled) {
+
+  //clear(grappled.layer)
+  // the idea is to animate the grappeled layer
+  var layer = grappled.ctx;
+  layer.clearRect(0, 0, 64, 64);
+
+  var star = {
+    x: 32,
+    y: 32,
+    size: 6,
+    strokeOffset: 16,
+    bounds: 64
+  }
+
+  layer.beginPath();
+  layer.arc(star.x, star.y, star.size, 0, Math.PI*2, true);
+  layer.closePath();
+  layer.fillStyle = 'white';
+  layer.fill();
+  layer.closePath();
+
+  // star stroke/progress
+  layer.beginPath();
+  layer.lineWidth = 1;
+  layer.strokeStyle = 'white';
+  layer.arc(star.x, star.y, star.size+star.strokeOffset, 0, Math.PI*2, true);
+  layer.closePath();
+  layer.stroke();
+
+  var radius = 23;
+  var startAngle = 2 * Math.PI;
+  //var endAngle = ring * Math.PI
+  var counterClockwise = true;
+
+  ring += 0.02;
+  if (ring <= 0) {
+    ring = 2;
+  }
+  endAngle = ring * Math.PI
+
+  layer.beginPath();
+  layer.lineWidth = 3;
+  layer.strokeStyle = 'red';
+  layer.arc(star.x, star.y, radius, startAngle, endAngle, counterClockwise);
+  layer.stroke();
+
+  // visual bounds
+  layer.beginPath();
+  layer.lineWidth = 1;
+  layer.strokeStyle = 'lime';
+  layer.rect(star.x-(star.bounds/2),star.y-(star.bounds/2),star.bounds,star.bounds);
+  layer.stroke();
+
+
 }
 
 
 
+// create grid and render it as a canvas.
+var gridImage;
+
+function drawGrid() {
+  var gridCanvas = document.createElement('canvas');
+      gridCanvas.width = canvas.width;
+      gridCanvas.height = canvas.height;
+  var gridContext = gridCanvas.getContext('2d');
+  var ctx = gridContext;
+
+  gridImage = gridCanvas;
+
+  // figure out how to draw a 5 column grid (vert) and repeat horizontally.
+  var grid = {
+    size: 64
+  }
+  var horizontal;
+  var vertical;
+  var positionX = 0;
+  var positionY = 0;
+  var order;
+
+  for (var i = 0; i < 12; i++) {
+    if (order === true) {
+      order = false;
+    } else {
+      order = true;
+    }
+    drawCol(ctx,positionX,order);
+    positionX += 64;
+  }
+
+}
 
 
+function drawCol(ctx,positionX,order) {
+ var positionY = 0;
+ var squareColor;
+ var order = order;
 
+  // even or odd
+  for (var i = 0; i < 5; i++) {
+    if (order === true) {
+      order = false;
+      squareColor = 'rgba(0,0,0,0.05)';
+    } else {
+      order = true;
+      squareColor = 'rgba(0,0,0,0.15)';
+    }
+    drawSquare(ctx,squareColor,64,positionX,positionY);
+    positionY+=64;
+  }
+}
 
-
+function drawSquare(ctx,color,size,positionX,positionY) {
+  ctx.beginPath();
+  ctx.rect(positionX,positionY,size,size)
+  ctx.fillStyle = color;
+  ctx.fill();
+}
 
 
 
